1e3912c6b0890656e5b354f7f3e4add21ccb01df
A S U S <sawungwulung7@gmail.com>
2022-08-17 22:37:10 +0800

    sekuensial

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Belajar Sekuensial
+print('ibu berkata "tolong belikan 1 botol susu, kalau ada telur beli 6 telur".')
+print('Anak berkata "OK".')
+print('Anak pergi ke kios')
+print('Anak bertanya di pemilik kios "Apakah ada susu?".')
+print('pemilik toko menjawab, "ada".')
+print('anak membeli 1 botol susu')
+print('Anak bertanya di pemilik kios "Apakah ada telur?".')
+print('pemilik toko menjawab, "ada".')
+print('anak membeli 6 butir telur')
+print('anak pulang ke rumah')
+print('anak menyerahkan hasil belanjaan kepada ibu')
+
