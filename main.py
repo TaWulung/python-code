8b786b4a30bbf1272fc273fa43fea650c897886b
A S U S <sawungwulung7@gmail.com>
2022-08-22 20:59:15 +0800

    percabangan

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-
-# Belajar Sekuensial
+# belajar sekuensial
 print('ibu berkata "tolong belikan 1 botol susu, kalau ada telur beli 6 telur".')
-print('Anak berkata "OK".')
-print('Anak pergi ke kios')
-print('Anak bertanya di pemilik kios "Apakah ada susu?".')
+print('anak berkata "OK".')
+print('anak pergi ke kios')
+print('anak bertanya di pemilik kios "Apakah ada susu?".')
 print('pemilik toko menjawab, "ada".')
-print('anak membeli 1 botol susu')
-print('Anak bertanya di pemilik kios "Apakah ada telur?".')
-print('pemilik toko menjawab, "ada".')
-print('anak membeli 6 butir telur')
-print('anak pulang ke rumah')
-print('anak menyerahkan hasil belanjaan kepada ibu')
 
 # done
+
+# belajar percabangan
+jumlah_botol_susu = 189
+jumlah_telur = 456
+if jumlah_botol_susu > 0:
+    print('anak membeli  1 botol susu')
+else:
+    print('anak tidak jadi membeli 1 botol susu')
+print('anak bertanya, "apakah ada telur"?')
+print('pemilik toko menjawab, "ada".')
+if jumlah_telur > 0:
+    print('anak membeli 6 butir telur')
