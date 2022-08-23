f9ab1110d691197a6a023e48aa361ed4158da6d4
A S U S <sawungwulung7@gmail.com>
2022-08-23 19:34:12 +0800

    percabangan

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ print('anak berkata "OK".')
 print('anak pergi ke kios')
 print('anak bertanya di pemilik kios "Apakah ada susu?".')
 print('pemilik toko menjawab, "ada".')
-
 # done
 
 # belajar percabangan
@@ -18,3 +17,6 @@ print('anak bertanya, "apakah ada telur"?')
 print('pemilik toko menjawab, "ada".')
 if jumlah_telur > 0:
     print('anak membeli 6 butir telur')
+print('anak pulang ke rumah')
+print('anak menyerahkan belanjaan kepada ibu')
+# done
