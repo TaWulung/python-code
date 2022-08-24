b920714c4ecced88ec182f9b41e5a91300bf4545
A S U S <sawungwulung7@gmail.com>
2022-08-24 11:03:57 +0800

    perulangan

---
+++ belajar_perulangan_while.py
@@ -0,0 +1,14 @@
+""""
+perulangan_dengan_while
+"""
+Jumlah_buku = 13
+print('ibu berkata, "baca semua bukumu".')
+
+Jumlah_buku_yang_sudah_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}')
+
+while Jumlah_buku_yang_sudah_dibaca < Jumlah_buku:
+    Jumlah_buku_yang_sudah_dibaca = Jumlah_buku_yang_sudah_dibaca + 1
+    print(f'buku ke {Jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}')
