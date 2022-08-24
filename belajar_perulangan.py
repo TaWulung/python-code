59eafcd9106f4b00c11836d0f2ddbfbb0057b8f8
A S U S <sawungwulung7@gmail.com>
2022-08-24 08:05:35 +0800

    perulangan_for

---
+++ belajar_perulangan.py
@@ -0,0 +1,13 @@
+"""
+perulangan_dengan_for
+"""
+Jumlah_buku = 13
+print('ibu berkata "baca semua bukumu".')
+
+Jumlah_buku_yang_sudah_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}')
+
+for Jumlah_buku_yang_sudah_dibaca in range(1, Jumlah_buku+1):
+    print(f'buku ke {Jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah dibaca {Jumlah_buku_yang_sudah_dibaca}')
